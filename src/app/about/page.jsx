76b6a036f2f872b10a78cc812753de715e1ad160
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-04 18:25:36 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,47 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600"> 🎬 About Us...</h1>
+      <p>
+        Welcome to MovieVerse 🎥 — your ultimate destination for discovering
+        movies, exploring details, and staying updated with the world of cinema.
+        MovieVerse is a movie display platform designed for movie lovers who
+        enjoy browsing films by genre, rating, popularity, and release year. Our
+        goal is to provide a simple, fast, and visually engaging experience for
+        users to explore movies effortlessly.
+      </p>
+      <br />
+      <p>
+        🌟 What We Offer
+        <ul>
+          <li>
+            🎞️ Browse Movies – Explore a wide range of movies from different
+            genres{" "}
+          </li>
+          <li>
+            {" "}
+            ⭐ Ratings & Reviews – View IMDb-style ratings and user feedback
+          </li>
+          <li>
+            📅 Latest Releases – Stay updated with newly released and trending
+            movies{" "}
+          </li>
+          <li>🔍 Smart Search – Find your favorite movies instantly</li>
+          <li>
+            📱Responsive Design – Enjoy a seamless experience on all devices
+          </li>
+        </ul>
+      </p>
+
+      <br />
+      <h3> 🚀 Our Mission</h3>
+      <p>
+        Our mission is to make movie discovery easy, enjoyable, and accessible
+        for everyone. We aim to build a platform that helps users quickly find
+        movies they love while offering clean design and smooth performance.
+      </p>
+    </div>
+  );
+};
+
+export default About;
