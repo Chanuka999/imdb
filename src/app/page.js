3ca443e781f693631599efe8df34b02309f92e79
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-05 18:37:09 +0530

    conect TMDB to project

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
+const API_KEY = process.env.API_KEY;
 import React from "react";
 
-const page = () => {
+const page = async ({ searchParams }) => {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `http://api.themoviedb.org/3${
+      genre === "fetchTrending" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+
   return (
     <div>
       <h1>Home</h1>
