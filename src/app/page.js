4a517e6e937554f4ddbbc76d49ff5900f5c73b44
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-07 10:17:45 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
 const API_KEY = process.env.API_KEY;
 import Resuts from "@/components/Resuts";
 import React from "react";
+import { resolve } from "styled-jsx/css";
 
 const page = async ({ searchParams }) => {
   const params = await searchParams;
   const genre = params.genre || "fetchTrending";
-  const res = await fetch(
-    `http://api.themoviedb.org/3${
-      genre === "fetchTrending" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const response = await fetch(
+        `http://api.themoviedb.org/3${
+          genre === "fetchTrending" ? `/movie/top_rated` : `/trending/all/week`
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        { next: { revalidate: 10000 } }
+      );
+      resolve(response);
+    }, 2000);
+  });
 
   const data = await res.json();
   if (!res.ok) {
