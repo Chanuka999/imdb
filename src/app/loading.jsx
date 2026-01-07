20a601e7d2455ebcdd4a612807f14057ef205e7a
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-07 10:17:37 +0530

    loading page created

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center">
+      <img src="spinner2.svg" alt="loading image" />
+    </div>
+  );
+};
+
+export default loading;
