e44247d6545a80dcf8905dd6010aefb149e3872e
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-10 10:28:17 +0530

    create SearchPage component for movie search functionality

---
+++ src/app/search/[searchTerm]/page.jsx
@@ -0,0 +1,21 @@
+import Resuts from "@/components/Resuts";
+import React from "react";
+
+const SearchPage = async ({ params }) => {
+  const searchTerm = (await params).searchTerm;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results = data.results;
+  return (
+    <div>
+      {results &&
+        results.length ===
+        <h1 className="text-center pt-6">No result found</h1>}
+      {results && <Resuts results={results} />}
+    </div>
+  );
+};
+
+export default SearchPage;
