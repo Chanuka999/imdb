a9b2eb1c97327ad43075d5d74964e0dc7e5ca6f6
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-10 11:29:07 +0530

    wrap NavbarItem components in Suspense for improved loading handling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-4">
-      <NavbarItem title="Trending" params="fetchTrending" />
-      <NavbarItem title="Top Rated" params="fetchTopRated" />
+      <Suspense fallback={null}>
+        <NavbarItem title="Trending" params="fetchTrending" />
+      </Suspense>
+      <Suspense fallback={null}>
+        <NavbarItem title="Top Rated" params="fetchTopRated" />
+      </Suspense>
     </div>
   );
 };
